a57b127a712be29b5bfd7c0f73d0b54c36c08cec
lukasz <lukasz.stojecki@o2.pl>
2021-02-12 23:06:30 +0100

    * adding an exception package

---
+++ src/main/java/com/example/sda/travelagencyservice/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.example.sda.travelagencyservice.exception;
+
+public class BadRequestException extends Exception {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
