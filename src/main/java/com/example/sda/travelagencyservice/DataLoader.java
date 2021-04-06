3655dafd11d08f0db85f9fb22f9922fc79a7d81f
lukasz <lukasz.stojecki@o2.pl>
2021-04-06 19:50:13 +0200

    *  data loader updated

--- src/main/java/com/example/sda/travelagencyservice/DataLoader.java
+++ src/main/java/com/example/sda/travelagencyservice/DataLoader.java
@@ -1,28 +1,86 @@
 package com.example.sda.travelagencyservice;
 
-
-import com.example.sda.travelagencyservice.dto.TripDto;
-import com.example.sda.travelagencyservice.model.Airport;
-import com.example.sda.travelagencyservice.model.City;
-import com.example.sda.travelagencyservice.model.Trip;
+import com.example.sda.travelagencyservice.model.Role;
+import com.example.sda.travelagencyservice.model.TripPurchase;
+import com.example.sda.travelagencyservice.model.User;
+import com.example.sda.travelagencyservice.repository.TripPurchaseRepository;
 import com.example.sda.travelagencyservice.repository.TripRepository;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+import com.example.sda.travelagencyservice.repository.UserRepository;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.ArrayList;
-import java.util.Collections;
 
-@Component
-public class DataLoader implements CommandLineRunner {
+@Configuration
+public class DataLoader {
 
-private TripRepository tripRepository;
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+    private TripPurchaseRepository tripPurchaseRepository;
+    private TripRepository tripRepository;
 
-    public DataLoader(TripRepository tripRepository) {
+
+    public DataLoader(UserRepository userRepository, PasswordEncoder passwordEncoder,
+                      TripPurchaseRepository tripPurchaseRepository, TripRepository tripRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.tripPurchaseRepository = tripPurchaseRepository;
         this.tripRepository = tripRepository;
     }
 
-    @Override
-    public void run(String... args) throws Exception {
+    @Bean
+    public InitializingBean initializingBean() {
+        return () -> {
+            if (userRepository.findAll().isEmpty()) {
+                userRepository.save(
+                        User.builder()
+                                .username("admin")
+                                .password(passwordEncoder.encode("admin1"))
+                                .email("admin@o2.pl")
+                                .roles(Role.ADMIN)
+                                .build()
+                );
+                userRepository.save(
+                        User.builder()
+                                .username("user")
+                                .password(passwordEncoder.encode("user1"))
+                                .email("user@o2.pl")
+                                .roles(Role.USER)
+                                .build()
+                );
 
+            }
+
+            if (tripPurchaseRepository.findAll().isEmpty()){
+                tripPurchaseRepository.save(TripPurchase.builder()
+                        .trip(tripRepository.findById(1l).orElseThrow())
+                        .user(userRepository.findById(1l).orElseThrow())
+                        .childQuantity(4)
+                        .adultsQuantity(4)
+                        .amount(2700)
+                        .build()
+                );
+                tripPurchaseRepository.save(TripPurchase.builder()
+                        .trip(tripRepository.findById(1l).orElseThrow())
+                        .user(userRepository.findById(1l).orElseThrow())
+                        .childQuantity(5)
+                        .adultsQuantity(5)
+                        .amount(3700)
+                        .build()
+                );
+                tripPurchaseRepository.save(TripPurchase.builder()
+                        .trip(tripRepository.findById(1l).orElseThrow())
+                        .user(userRepository.findById(2l).orElseThrow())
+                        .childQuantity(3)
+                        .adultsQuantity(3)
+                        .amount(2400)
+                        .build()
+                );
+            }
+        };
     }
+
 }
+
+
