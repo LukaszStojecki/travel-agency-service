451817fa2bd851ffca1e101dbd2d3c60379026ff
lukasz <lukasz.stojecki@o2.pl>
2022-01-24 21:20:19 +0100

    *  registration bug fixed

--- src/main/java/com/example/sda/travelagencyservice/repository/UserRepository.java
+++ src/main/java/com/example/sda/travelagencyservice/repository/UserRepository.java
@@ -2,17 +2,18 @@ package com.example.sda.travelagencyservice.repository;
 
 
 import com.example.sda.travelagencyservice.model.User;
-import org.checkerframework.checker.nullness.Opt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
     Optional<User> findByEmail(String email);
 
     boolean existsByUsername(String username);
 
+    boolean existsByEmail(String email);
 }
